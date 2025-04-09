d29137ecb49913c1200b24ec78ff7ee8593a5faf
vijay <vjblacklight@gmail.com>
2025-04-09 09:36:05 +0530

    ADD - problem 37: sudoku_solver (accepted)

--- backtracking/sudoku_solver/sudoku_solver.py
+++ backtracking/sudoku_solver/sudoku_solver.py
@@ -1,5 +1,6 @@
 from typing import List, Set
 from collections import Counter, deque
+from time import perf_counter
 
 
 class Solution:
@@ -9,6 +10,7 @@ class Solution:
         """
         rows: List[Set[str]] = [set() for _ in range(len(board))]
         columns: List[Set[str]] = [set() for _ in range(len(board[0]))]
+        subgrid: List[Set[str]] = [set() for _ in range(len(board[0]))]
         empty_cells = deque()
 
         for i in range(9):
@@ -18,12 +20,14 @@ class Solution:
                     continue
                 rows[i].add(board[i][j])
                 columns[j].add(board[i][j])
+                self.__insertInSubgrid(subgrid, i, j, board[i][j])
+
         # print(f"empty cells:{empty_cells}")
 
         if len(empty_cells) > 0:
-            self.__solve(board, rows, columns, empty_cells)
+            self.__solve(board, rows, columns, subgrid, empty_cells)
         # print(f"solution found:{solved}")
-        # self.__printBoard(board)
+        self.__printBoard(board)
         # print(self.__checkSudoku(board))
         # print(rows)
         # print(columns)
@@ -33,88 +37,92 @@ class Solution:
         board: List[List[str]],
         rows: List[Set[str]],
         cols: List[Set[str]],
+        subgrid: List[Set[str]],
         empty_cells,
     ) -> bool:
         if len(empty_cells) == 0:
-            return self.__checkSudoku(board)
+            return True
 
         next_empty_pos = empty_cells.popleft()
         i, j = next_empty_pos
 
         for m in range(1, 10):
-            if self.__checkused(str(m), board, rows, cols, i, j):
+            if self.__checkused(str(m), rows, cols, subgrid, i, j):
                 continue
             board[i][j] = str(m)
-            self.__markUsed(board[i][j], rows, cols, i, j)
+            self.__markUsed(board[i][j], rows, cols, subgrid, i, j)
 
             if self.__solve(
                 board,
                 rows,
                 cols,
+                subgrid,
                 empty_cells,
             ):
                 return True
 
-            self.__markFree(board[i][j], rows, cols, i, j)
+            self.__markFree(board[i][j], rows, cols, subgrid, i, j)
             board[i][j] = "."
 
         empty_cells.appendleft(next_empty_pos)
         return False
 
-    def __checkSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(9):
-            counter = Counter(board[i])
-            if len(counter) != 9 or "." in counter:
-                # print("here1")
-                return False
-        for j in range(9):
-            counter = Counter([board[i][j] for i in range(9)])
-            if len(counter) != 9 or "." in counter:
-                # print("here2")
-                return False
-
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                val_map = {}
-                for l in range(i, i + 3):  #
-                    for k in range(j, j + 3):
-                        if board[l][k] in val_map or board[l][k] == ".":
-                            # print("here3")
-                            return False
-                        val_map[board[l][k]] = True
-
-        return True
-
     def __checkused(
         self,
         num: str,
-        board: List[List[str]],
         rows: List[Set[str]],
         columns: List[Set[str]],
+        subgrid: List[Set[str]],
         i: int,
         j: int,
     ) -> bool:
         if num in rows[i] or num in columns[j]:
             return True
-        subgrid_i = 3 * (i // 3)
-        subgrid_j = 3 * (j // 3)
-        for l in range(subgrid_i, subgrid_i + 3):
-            for m in range(subgrid_j, subgrid_j + 3):
-                if board[l][m] == num:
-                    return True
-        return False
+        return self.__checkInSubgrid(subgrid, i, j, num)
 
     def __markUsed(
-        self, num: str, rows: List[Set[str]], columns: List[Set[str]], i: int, j: int
+        self,
+        num: str,
+        rows: List[Set[str]],
+        columns: List[Set[str]],
+        subgrid: List[Set[str]],
+        i: int,
+        j: int,
     ) -> None:
         rows[i].add(num)
         columns[j].add(num)
+        self.__insertInSubgrid(subgrid, i, j, num)
 
     def __markFree(
-        self, num: str, rows: List[Set[str]], columns: List[Set[str]], i: int, j: int
+        self,
+        num: str,
+        rows: List[Set[str]],
+        columns: List[Set[str]],
+        subgrid: List[Set[str]],
+        i: int,
+        j: int,
     ) -> None:
         rows[i].remove(num)
         columns[j].remove(num)
+        self.__removeFromSubgrid(subgrid, i, j, num)
+
+    def __insertInSubgrid(
+        self, subgrid: List[Set[str]], i: int, j: int, num: str
+    ) -> None:
+        subgrid_ind = 3 * (i // 3) + j // 3
+        subgrid[subgrid_ind].add(num)
+
+    def __removeFromSubgrid(
+        self, subgrid: List[Set[str]], i: int, j: int, num: str
+    ) -> None:
+        subgrid_ind = 3 * (i // 3) + j // 3
+        subgrid[subgrid_ind].remove(num)
+
+    def __checkInSubgrid(
+        self, subgrid: List[Set[str]], i: int, j: int, num: str
+    ) -> bool:
+        subgrid_ind = 3 * (i // 3) + j // 3
+        return True if num in subgrid[subgrid_ind] else False
 
     def __printBoard(self, board):
         for i in range(len(board)):
@@ -122,6 +130,7 @@ class Solution:
 
 
 sol = Solution()
+c1_s = perf_counter()
 sol.solveSudoku(
     [
         ["5", "3", ".", ".", "7", ".", ".", ".", "."],
@@ -135,3 +144,20 @@ sol.solveSudoku(
         [".", ".", ".", ".", "8", ".", ".", "7", "9"],
     ]
 )
+print(perf_counter() - c1_s)
+print("-------------------------------------------------------------------------")
+c2_s = perf_counter()
+sol.solveSudoku(
+    [
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", "9", ".", ".", "1", ".", ".", "3", "."],
+        [".", ".", "6", ".", "2", ".", "7", ".", "."],
+        [".", ".", ".", "3", ".", "4", ".", ".", "."],
+        ["2", "1", ".", ".", ".", ".", ".", "9", "8"],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", "2", "5", ".", "6", "4", ".", "."],
+        [".", "8", ".", ".", ".", ".", ".", "1", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    ]
+)
+print(perf_counter() - c2_s)
